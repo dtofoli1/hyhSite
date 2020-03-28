9aa36f09f2ba048373bb71adc79db2a7e2d5b359
dtofoli1 <daltontofoli1@gmail.com>
2020-03-28 03:04:23 -0300

    JQuery Wrap
    Wrapped requests in JQuery, getting 406 on POST, checking that later.

--- src/request.js
+++ src/request.js
@@ -2,6 +2,7 @@ $("#main").ready(function() {
 	$.ajax({
 		method: "GET",
 		url: "https://haveyouheard-game.herokuapp.com/get_topics",
+		mode: "no-cors",
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Content-Type": "application/json"
@@ -17,6 +18,40 @@ $("#main").ready(function() {
 });
 
 $("#sendform").click(function() {
-	var formData = JSON.stringify($("#fullform").serialize());
-	console.log(formData);
+	var $completText = $("#gameNews");
+	var $url = $("#newsLink");
+
+	var postData = {
+		complete_text: $completText.val(),
+		incomplete_text: $completText.val(),
+		id_topic: 4,
+		url: $url.val()
+	};
+	console.log(postData);
+
+	$.ajax({
+		type: "POST",
+		url: "https://haveyouheard-game.herokuapp.com/add_news",
+		contentType: "text/html; charset=utf-8",
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			Accept: "*/*"
+		},
+		data: {
+			complete_text:
+				"Livro do (‘menino do Acre’) entra na lista dos mais vendidos no Brasil",
+			incomplete_text:
+				"Livro do ________ entra na lista dos mais vendidos no Brasil",
+			id_topic: 4,
+			url:
+				"https://brasil.estadao.com.br/noticias/geral,livro-do-menino-do-acre-entra-na-lista-dos-livros-mais-vendidos,70001924841"
+		},
+		success: function() {
+			alert("Obrigado por Contribuir!");
+			console.log(postData);
+		},
+		error: function() {
+			alert("Algo não deu certo, tente novamente!");
+		}
+	});
 });
